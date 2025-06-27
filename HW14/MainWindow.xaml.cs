376d34e15339df91199977c5ca65c1eb6f7842b1
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2025-06-27 14:40:59 +0300

    HW14?!?!?!?!

---
+++ HW14/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HW14
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbOption1.IsChecked == true)
+                textSelectedVariant.Text = "Выбран: Белый кот";
+            else if (rbOption2.IsChecked == true)
+                textSelectedVariant.Text = "Выбран: Котёнок который устал";
+            else if (rbOption3.IsChecked == true)
+                textSelectedVariant.Text = "Выбран: Китайский котек";
+        }
+
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder("Включено: ");
+            if (cbOptionA.IsChecked == true) sb.Append("Поспал плохо ");
+            if (cbOptionB.IsChecked == true) sb.Append("SmileFace ");
+            if (cbOptionC.IsChecked == true) sb.Append("Не довольный? ");
+            textSelectedOptions.Text = sb.ToString().Trim();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            rbOption1.IsChecked = rbOption2.IsChecked = rbOption3.IsChecked = false;
+            cbOptionA.IsChecked = cbOptionB.IsChecked = cbOptionC.IsChecked = false;
+            textSelectedVariant.Text = "";
+            textSelectedOptions.Text = "";
+        }
+    }
+}
