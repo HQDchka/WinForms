b35808ccdc275cc77bb29756bfb4e4ff3c2a448c
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2025-06-30 05:15:49 +0300

    Paint?🖌

---
+++ MyPaint/MyPaint/Form3.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace MyPaint
+{
+    public partial class Form3 : Form
+    {
+        Form1 Fm;
+        int X, Y, W, H;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Fm.H = -1;
+            Fm.W = -1;
+            Fm.Y = -1;
+            Fm.X = -1;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            X = (int)numericUpDownX.Value;
+            Y = (int)numericUpDownY.Value;
+            W = (int)numericUpDown2.Value;
+            H = (int)numericUpDown1.Value;
+
+            Fm.X = X;
+            Fm.Y = Y;
+            Fm.W = W;
+            Fm.H = H;
+
+            if (Fm.ActiveMenuItem != null)
+            {
+                Graphics g = Fm.CreateGraphics();
+
+                switch (Fm.ActiveMenuItem)
+                {
+                    case "контурToolStripMenuItem":
+                        Fm.DrawRectangl(g, X, Y, W, H);
+                        break;
+                    case "заливкаToolStripMenuItem":
+                        Fm.DrawRectanglFill(g, X, Y, W, H);
+                        break;
+                    case "контурToolStripMenuItem1":
+                        Fm.DrawEllipse(g, X, Y, W, H);
+                        break;
+                    case "заливкаToolStripMenuItem1":
+                        Fm.DrawEllipseFull(g, X, Y, W, H);
+                        break;
+                    case "контурToolStripMenuItem2":
+                        Fm.DrawPolygon(g);
+                        break;
+                    case "заливкаToolStripMenuItem2":
+                        Fm.DrawPolygonFill(g);
+                        break;
+                    case "контурToolStripMenuItem3":
+                        Fm.DrawPies(g, X, Y, W, H);
+                        break;
+                    case "заливкаToolStripMenuItem3":
+                        Fm.DrawPiesFill(g, X, Y, W, H);
+                        break;
+                    case "дугаToolStripMenuItem":
+                        Fm.DrawArc(g, X, Y, W, H);
+                        break;
+                    case "сплошнаяToolStripMenuItem":
+                        Fm.DrawSolidLine(g, X, Y, W, H);
+                        break;
+                    case "прерывистаяToolStripMenuItem":
+                        Fm.DrawDashLine(g, X, Y, W, H);
+                        break;
+                    case "пунктирнаяToolStripMenuItem":
+                        Fm.DrawDashDotLine(g, X, Y, W, H);
+                        break;
+                }
+                g.Dispose();
+            }
+            this.Close();
+        }
+
+        public Form3(Form1 fm)
+        {
+            InitializeComponent();
+            Fm = fm;
+        }
+    }
+}
---
+++ MyPaint/MyPaint/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("MyPaint")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyPaint")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7fdd9ae7-c60a-4ae8-ba62-ad9649670618")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
